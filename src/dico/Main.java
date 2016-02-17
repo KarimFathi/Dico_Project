812053e68b70fce5fc48cac657afb629f013cd5c
Karim <karim.fathi@outlook.com>
2016-02-17 03:34:41 +0100

    Update Main.java

--- src/dico/Main.java
+++ src/dico/Main.java
@@ -1,6 +1,5 @@
 /*
  * Karim Fathi
- * Salim Belkebir 
  */
 package dico;
 
@@ -12,29 +11,29 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("---------------------------------1-------------------------------------------");
-		System.out.println("Veuillez saisir la taille du dictionnaire (Il faut que ça soit un entier) :");
+		System.out.println("Veuillez saisir la taille du dictionnaire (Il faut que Ã§a soit un entier) :");
 		int i = sc.nextInt();
 		System.out.println("Instancier un dictionnaire de longueur : " + i);
 		OrderedDictionary dd = new OrderedDictionary(i);
 		System.out.println("\nAffichage du contenu du dictionnaire : " + dd.toString());
 		System.out.println("\nCe dictionnaire est-il vide : " + dd.isEmpty());
-		System.out.println("Nombre de couple clé - valeur éfectivement contenu dans le dictionnaire : " + dd.size());
+		System.out.println("Nombre de couple clÃ© - valeur Ã©fectivement contenu dans le dictionnaire : " + dd.size());
 		
 		System.out.println("\n\n---------------------------------2-------------------------------------------");
-		dd.put("vache", "animale doméstique");
+		dd.put("vache", "animale domÃ©stique");
 		System.out.println("\nAffichage du contenu du dictionnaire : " + dd.toString());
 		System.out.println("\nCe dictionnaire est-il vide : " + dd.isEmpty());
-		System.out.println("Nombre de couple clé - valeur éfectivement contenu dans le dictionnaire : " + dd.size());
-		System.out.println("Donnez la définition du mot 'cargo' : " + dd.get("cargo"));
+		System.out.println("Nombre de couple clÃ© - valeur Ã©fectivement contenu dans le dictionnaire : " + dd.size());
+		System.out.println("Donnez la dÃ©finition du mot 'cargo' : " + dd.get("cargo"));
 		
 		System.out.println("\n\n---------------------------------3-------------------------------------------");
 		dd.put("rose", "fleur du rosier");
 		dd.put("cargo", "navire de charge");
-		dd.put("mississipi", "fleuve se jetant dans le golf du Méxique");
+		dd.put("mississipi", "fleuve se jetant dans le golf du MÃ©xique");
 		System.out.println("\nAffichage du contenu du dictionnaire : " + dd.toString());
 		System.out.println("\nCe dictionnaire est-il vide : " + dd.isEmpty());
-		System.out.println("Nombre de couple clé - valeur éfectivement contenu dans le dictionnaire : " + dd.size());
-		System.out.println("Donnez la définition du mot 'cargo' : " + dd.get("cargo"));
+		System.out.println("Nombre de couple clÃ© - valeur Ã©fectivement contenu dans le dictionnaire : " + dd.size());
+		System.out.println("Donnez la dÃ©finition du mot 'cargo' : " + dd.get("cargo"));
 	}
 
 }
